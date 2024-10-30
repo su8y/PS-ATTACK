33a243c358f33274f97d289cf7c02c0860446d5f
SUA BAE <90668202+su8y@users.noreply.github.com>
2024-10-30 20:22:11 +0900

    23871_ZOAC 4.cpp

---
+++ korsua/solved.ac/23871_ZOAC 4.cpp
@@ -0,0 +1,27 @@
+/*
+https://www.acmicpc.net/problem/23971
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m,w,h;
+
+int main(){
+    cin >> h >> w >> n >> m;
+    int ret = 0;
+    int h_count=0, w_count=0;
+    for(int i = 0 ; i < h; i += (n+1)) h_count++;
+    for(int i = 0 ; i < w; i += (m+1)) w_count++;
+  
+    if(!h_count && !w_count) {
+        ret = 0;
+    } else if( !h_count || !w_count){
+         ret = h_count == 1 ? h_count : w_count;
+    } else {
+        ret = h_count * w_count;
+    }
+    printf("%d\n", ret);
+  
+    return 0;
+}
+
