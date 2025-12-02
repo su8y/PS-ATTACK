91426a89d2a16ec5b4f419430f6742c2004b2777
tecte <121792901+tecte1023@users.noreply.github.com>
2025-12-02 12:15:22 +0900

    Tecte / 6 문제 (#2)
    * chore: 기존 디렉토리 구조 삭제
    
    * chore: tecte 모듈 추가 및 .gitignore 설정 통합
    
    * refactor: 패키지 구조 개선
    
    * [BOJ]3653. 영화 수집
    
    * [BOJ]16499. 동일한 단어 그룹화하기
    
    * [BOJ]14650. 걷다보니 신천역 삼 (Small)
    
    * [BOJ]9575. 행운의 수
    
    * [BOJ]1719. 택배
    
    * [BOJ] 11497. 통나무 건너뛰기

---
+++ tecte/src/main/java/baekjoon/workbook0/chapter2/section9/BOJ_2920_음계.java
@@ -0,0 +1,28 @@
+/*
+ * 문제: 백준 2920번 - 음계 (https://www.acmicpc.net/problem/2920)
+ *
+ * 시간 복잡도: O(1)
+ */
+
+package baekjoon.workbook0.chapter2.section9;
+
+import java.io.*;
+
+public class BOJ_2920_음계 {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        String result = "mixed";
+
+        if (input.equals("1 2 3 4 5 6 7 8"))
+            result = "ascending";
+        else if (input.equals("8 7 6 5 4 3 2 1"))
+            result = "descending";
+
+        bw.write(result);
+        br.close();
+        bw.close();
+    }
+}
---
+++ tecte/src/main/java/baekjoon/workbook1/chapter5/section7/BOJ_15740_AB9.java
@@ -0,0 +1,25 @@
+/*
+ * 문제: 백준 15740번 - A+B - 9 (https://www.acmicpc.net/problem/15740)
+ *
+ * 시간 복잡도: O(n) - n: 수의 자릿수
+ */
+
+package baekjoon.workbook1.chapter5.section7;
+
+import java.io.*;
+import java.math.*;
+
+public class BOJ_15740_AB9 {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] num = br.readLine().split(" ");
+        BigInteger a = new BigInteger(num[0]);
+        BigInteger b = new BigInteger(num[1]);
+
+        bw.write(a.add(b).toString());
+        br.close();
+        bw.close();
+    }
+}
